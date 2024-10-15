d65af595b3a1a0815fcb35e3de1633d8a255a00c
Hamster <100172953+Hamster003@users.noreply.github.com>
2024-10-15 10:30:49 -0500

    Started working in Linux script

--- Linux/RunAsRoot.sh
+++ Linux/RunAsRoot.sh
@@ -1,2 +1,60 @@
 #!/bin/bash
-echo "This script is designed for Ubuntu and may not work on other Distros"
+
+if [ $(id -u) -ne 0 ] #Checks to see if the User ID is 0 (Root) if no it exits the script
+  then echo Please run this script as root or using sudo!
+  exit
+fi
+
+echo "This script is designed for Ubuntu with a SystemD init process and may not work on other Distros"
+
+echo "Time for updates"
+apt update && apt upgrade
+
+select () {
+echo "Choose an option "
+echo "1-User Managment    2-Password Policy"
+echo "3-Firewall          4-Updates"
+echo "5-Software          99-Exit"
+echo " "
+read mainselec
+
+if [$mainselec -eq 1]
+    then usermanage
+elif [$mainselec -eq 2]
+    then passpolicy
+elif [$mainselec -eq 3]
+    then firewall
+elif [$mainselec -eq 4]
+    then update
+elif [$mainselec -eq 5]
+    then software
+elif [$mainselec -eq 99]
+    then echo "Goodbye"
+    exit
+else []
+    then select
+}
+usermanage () {
+
+}
+
+passpolicy () {
+echo "copying old files as a backup"
+mkdir ./pam.d
+cp /etc/login.defs ./login.defs
+cp /etc/pam.d/common-auth ./pam.d/common-auth
+
+
+}
+
+firewall () {
+
+}
+
+update () {
+
+}
+
+software () {
+
+}
